c20c1d1bfa65ff88a3edda4f7738f4354b46cc78
joechaiwat <joe4206079@gmail.com>
2023-12-29 18:57:35 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('app:create-scrap-cron')->everyMinute();
+        // $schedule->command('app:create-scrap-cron')->everyMinute();
+        $schedule->command('app:create-scrap-cron')->everyFiveMinutes();
     }
 
     /**
