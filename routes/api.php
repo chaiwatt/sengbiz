b0beb38964455b6503a8c3b073cd308e898cc545
joechaiwat <joe4206079@gmail.com>
2024-01-16 12:17:31 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('webhook', [FacebookWebhookController::class, 'webhook'])->name('webhook');
+Route::get('webhook', [FacebookWebhookController::class, 'webhook'])->name('webhook');
