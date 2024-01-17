0038144c00bba9ec74c7adb18b33c69d90129c94
joechaiwat <joe4206079@gmail.com>
2024-01-17 16:04:54 +0700

    update

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -20,48 +20,40 @@
     </a>
     @endif
 
-    {{-- Pagination Elements --}}
+
     @foreach ($elements as $element)
-    {{-- Array Of Links --}}
     @if (is_array($element))
-    @php
-    $totalPages = $paginator->lastPage();
-    @endphp
-
     @foreach ($element as $page => $url)
-    @if ($page <= 5 || $page===$totalPages) @if ($page==$paginator->currentPage())
-        <span class="page-numbers current">{{ $page }}</span>
-        @else
-        <a class="page-numbers" href="{{ $url }}">{{ $page }}</a>
-        @endif
-        @elseif ($page == 6)
-        <span class="page-numbers">...</span>
-        @endif
-        @endforeach
-        @endif
-        @endforeach
+    @if ($page == $paginator->currentPage())
+    <span class="page-numbers current">{{ $page }}</span>
+    @else
+    <a class="page-numbers" href="{{ $url }}">{{ $page }}</a>
+    @endif
+    @endforeach
+    @endif
+    @endforeach
 
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
+    {{-- Next Page Link --}}
+    @if ($paginator->hasMorePages())
 
-        <a class="next page-numbers" href="{{ $paginator->nextPageUrl() }}">
-            ถัดไป
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-            </svg>
-        </a>
-        @else
+    <a class="next page-numbers" href="{{ $paginator->nextPageUrl() }}">
+        ถัดไป
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+            class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+                d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+        </svg>
+    </a>
+    @else
 
-        <a class="next page-numbers" href="#">
-            ถัดไป
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-            </svg>
-        </a>
-        @endif
+    <a class="next page-numbers" href="#">
+        ถัดไป
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+            class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+                d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+        </svg>
+    </a>
+    @endif
 </div>
 @endif
