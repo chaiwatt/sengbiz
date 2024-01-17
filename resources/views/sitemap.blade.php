e2b7489d4a22f43b2b23231cb1672332b7429250
joechaiwat <joe4206079@gmail.com>
2024-01-17 21:16:31 +0700

    update

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,10 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{url('/')}}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
     @foreach ($posts as $post)
     <url>
         <loc>{{url('/')}}/{{$post->slug}}</loc>
