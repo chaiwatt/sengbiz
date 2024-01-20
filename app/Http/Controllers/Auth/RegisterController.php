8a1412eaf9128588622c800de12f73d05a463f31
joechaiwat <joe4206079@gmail.com>
2024-01-20 15:44:38 +0700

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        dd('ok');
+
         return User::create([
             'name' => $data['name'],
             'owner_id' => $data['owner'],
