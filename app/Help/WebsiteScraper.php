addc889a95f9fa4f34fa770899e9a2aa75f58b65
joechaiwat <joe4206079@gmail.com>
2024-01-21 16:33:34 +0700

    update

--- app/Help/WebsiteScraper.php
+++ app/Help/WebsiteScraper.php
@@ -446,7 +446,7 @@ class WebsiteScraper
                 Scraped::create(['pid' => $orgPostId]);
 
                 IndexingPost::dispatch($post->id,$post->slug)->onQueue('indexing');
-                // FacebookPost::dispatch($post->id)->onQueue('facebookpost');
+                FacebookPost::dispatch($post->id)->onQueue('facebookpost');
 
                 // dd($orgUser, $orgPostId ,$orgTitle,$price,$categories,$locations,$links,$nears,$coordinates,$result,$postTitle,$postDesctiption,$phoneNumbers,$stringContent,$htmlContent);
             }
