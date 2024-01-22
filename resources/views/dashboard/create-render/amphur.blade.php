c44cc710bafd1b7d5b65639f2ee6ac04fa9a71cf
joechaiwat <joe4206079@gmail.com>
2024-01-22 15:27:51 +0700

    update

--- resources/views/dashboard/create-render/amphur.blade.php
+++ resources/views/dashboard/create-render/amphur.blade.php
@@ -1,6 +1,6 @@
 @if ($amphurs->count() != 0)
 <div class="">
-    <label class="required fw-medium mb-2">หมวดหมู่รอง</label>
+    <label class="required fw-medium mb-2">เลือกอำเภอ</label>
     <select class="form-select" name="amphur" id="amphur">
         <option value="{{null}}">==เลือกอำเภอ==</option>
         @foreach ($amphurs as $amphur)
