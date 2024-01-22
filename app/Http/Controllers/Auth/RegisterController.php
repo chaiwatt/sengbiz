8cae6093ec15ec72868fe3a1f4e103b677625161
joechaiwat <joe4206079@gmail.com>
2024-01-22 15:23:02 +0700

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -57,7 +57,6 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
-        dd($validation);
         return $validation;
     }
 
