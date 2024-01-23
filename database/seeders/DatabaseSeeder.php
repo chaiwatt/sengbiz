88b347afb5cae4bb7c51346295e25ba523da9018
joechaiwat <joe4206079@gmail.com>
2024-01-23 13:17:15 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,15 +20,15 @@ class DatabaseSeeder extends Seeder
         // ]);
         
         $this->call(NearPlacesTableSeeder::class);
-        // $this->call(PostPackagesTableSeeder::class);
-        // $this->call(PriceRangesTableSeeder::class);
-        // $this->call(ProvincesTableSeeder::class);
-        // $this->call(AmphursTableSeeder::class);
-        // $this->call(MainCategoriesTableSeeder::class);
-        // $this->call(SubCategoriesTableSeeder::class);
-        // $this->call(SubMinorCategoriesTableSeeder::class);
-        // $this->call(PriceRangesTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
+        $this->call(PostPackagesTableSeeder::class);
+        $this->call(PriceRangesTableSeeder::class);
+        $this->call(ProvincesTableSeeder::class);
+        $this->call(AmphursTableSeeder::class);
+        $this->call(MainCategoriesTableSeeder::class);
+        $this->call(SubCategoriesTableSeeder::class);
+        $this->call(SubMinorCategoriesTableSeeder::class);
+        $this->call(PriceRangesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     
     }
 }
