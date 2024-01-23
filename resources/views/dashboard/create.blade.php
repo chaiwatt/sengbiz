a62a02b3449146d0fecb0ede14cfd4929513c85b
joechaiwat <joe4206079@gmail.com>
2024-01-23 13:32:39 +0700

    update

--- resources/views/dashboard/create.blade.php
+++ resources/views/dashboard/create.blade.php
@@ -63,7 +63,7 @@
                 </div>
                 <div class="card mb-4">
                     <div class="card-header position-relative">
-                        <h6 class="fs-17 fw-semi-bold mb-0">Google Map</h6>
+                        <h6 class="fs-17 fw-semi-bold mb-0">สถานที่ตั้ง</h6>
                     </div>
                     <div class="card-body">
                         <div class="row g-4">
