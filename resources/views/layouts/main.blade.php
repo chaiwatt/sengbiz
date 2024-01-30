8db89e1ed22a042a2d61a8ef57bfaaab7fd3c46d
joechaiwat <joe4206079@gmail.com>
2024-01-30 21:43:56 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>ประกาศฟรี เซ้งกิจการ ขายกิจการ ขายอสังหาริมทรัพท์ | เซ้งกิจการ SengBiz ddd</title>
+    <title>ประกาศฟรี เซ้งกิจการ ขายกิจการ ขายอสังหาริมทรัพท์ | เซ้งกิจการ SengBiz</title>
     <meta name="description"
         content="ประกาศฟรี เซ้งกิจการ ขายกิจการ ขายอสังหาริมทรัพท์ ขายคอนโด ขายบ้านเดี่ยว ขายทาว์นโฮม ขายตึกแถว/อาคารพาณิชย์ ขายที่ดิน ขายแฟรนไชส์">
     <meta name="keywords"
