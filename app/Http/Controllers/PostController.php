3aaadc49ac3441b604a6b51ce874270d2b7bd0bc
joechaiwat <joe4206079@gmail.com>
2024-02-02 07:11:36 +0700

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -46,8 +46,8 @@ class PostController extends Controller
 
     public function index()
     {
-        $mainCategories = MainCategory::with('posts')->get();
-        dd($mainCategories);
+        // $mainCategories = MainCategory::with('posts')->get();
+        // dd($mainCategories);
         $provinces = Province::all();
         $mainCategories = MainCategory::all();
         
